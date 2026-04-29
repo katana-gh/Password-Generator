47ed703e82632a37850b75ff8d7e1c50d02596fc
katana-gh <royxalvarez@gmail.com>
2026-04-28 22:55:29 -0500

    add file handling to main

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from get_password import get_password
 from sys import argv
 
 def main():
+    
     value = argv[1]
+
     get_password(value)
 
-main()
+try:
+    num = int(argv[1])
+except ValueError:
+    print("argument must be a numeric value")
+except IndexError:
+    print("please enter an argument")
+else:
+    main()
